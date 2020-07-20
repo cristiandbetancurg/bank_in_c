c51af6ccc27209b47da692905ff315759d0dc343
cristiandbetancurg <cristiandbetancurg@gmail.com>
2020-07-19 22:35:21 -0400

    main_bank

---
+++ main-bank.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main ()
+
+{
+    int saldo;   
+    int deposito;
+    int pregunta;
+
+    printf("\n\n....::::Bienvenido a su Portal de Mi-Banco el Línea::::....\n\n\n\n¿Qué desea hacer?\n\nPresione:\n 1-Consultar Saldo\n2-Depositar Fondos\n3-Trasnferir Fondos\n4-Pagar con Débito\nIngrese su opción:");
+    
+    scanf("%d",&pregunta);
+
+    if (pregunta == 2){
+       printf("Ingrese el monto a depositar\n");    
+    }else{
+        printf("intente de nuevo\n");
+        scanf("%d",&pregunta);
+
+        if (pregunta == 2){
+             printf("Ingrese el monto a depositar\n");    
+        }
+    
+    }    
+    
+    return 0;
+
+}
